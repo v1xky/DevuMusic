9b396f559c6cb756a9ac8aca6b3afabdcf4fcbac
/x-ᴄᴏᴅᴇʀ <72803194+HYPER-AD@users.noreply.github.com>
2022-06-13 16:40:52 +0530

    Update string.py

--- cache/stuffs/string.py
+++ cache/stuffs/string.py
@@ -1,1 +1,42 @@
+# Powered by @HYPER_AD13 | @ShiningOff
+# Dear Pero ppls Plish Don't remove this line from here🌚
+
+# Powered by @HYPER_AD13 | @ShiningOff
+# Dear Pero ppls Plish Don't remove this line from here🌚
+
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+
+
+START_TEXT = """Hey {}\n\n➥ Myself {}\n❅ A simple , lagfree and flexible music robot!\n❅ If you facing any issue related to this music bot then please join @{}\n❅ For more help you can explorer help menu by tapping on /help !"""
+
+
+HELP_TXT = """Hoi {}\nHere is the help menu choose your desireoption nd explorer it!!\n\nFor any kind of help or query Just join @{} and ask your query!!"""
+
+
+B_HELP = """
+`Basics Commands !!`
+
+/play (query, ytlink, audio file) - use this command and enjoy music
+/ytp (query) - Use it for better search music!!
+/song (query) - Download your favourite songs using this command!
+/search (query) - This command will give you youtube search of your query!
+"""
+
+A_HELP = """
+`Admins Commands!!`
+
+/pause - To pause the song!
+/resume - Resume paused song!
+/skip - skip to the next song!
+/end - End the stream!
+/joinub - To invite assistant in your group!
+
+
+`Sudo Command!`
+
+/rmf - To clean Download file from database!
+/rmw - To clean raw files from database!
+/dclean - To clean files from server!
+"""
+
 
