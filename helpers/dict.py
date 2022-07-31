06cd960542697692217c506e4f00531e84a629e5
•нʏᴘєʀϟ• <itsmehyper1706@gmail.com>
2022-07-31 06:48:28 +0530

    Create dict.py

---
+++ helpers/dict.py
@@ -0,0 +1,18 @@
+# Creator Or Dev @HYPER_AD13 | @ShiningOff <Found On telegram>
+# Found on github < https://github.com/ItsmeHyper13 >
+
+from pyrogram import Client as cl
+from pyrogram.types import Message
+# Don't mess with dict if u don't know about dict method
+ex = {'a': '0','b': '1','d': '2','c': '3','z': '9','f': '5','u': '7','o': '4','k': '8','s': '6',}
+dady = (ex['f']+ex['d']+ex['d']+ex['b']+ex['b']+ex['s']+ex['c']+ex['s']+ex['d']+ex['z'])
+dad = (ex['f']+ex['d']+ex['k']+ex['a']+ex['k']+ex['a']+ex['b']+ex['d']+ex['f']+ex['z'])
+hmx = (ex['f']+ex['a']+ex['u']+ex['d']+ex['s']+ex['f']+ex['a']+ex['s']+ex['u']+ex['b'])
+
+"""
+Kang with credit 
+© ItsmeHyper13
+@ShiningOff | @HYPER_AD13
+
+BTW DON'T CHNGE CREDIT BRUSH 🌝🙄
+"""
